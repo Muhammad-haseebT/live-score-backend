f49be8a5f0bd2af8a993a9115f39cc1778631285
Muhammad-haseebT <mht34579@gmail.com>
2025-10-10 17:53:49 +0500

    Login/Register Feature added

--- src/main/java/com/livescore/backend/Controller/AccountController.java
+++ src/main/java/com/livescore/backend/Controller/AccountController.java
@@ -4,18 +4,20 @@ import com.livescore.backend.Entity.Account;
 import com.livescore.backend.Service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
 public class AccountController {
     @Autowired
     AccountService as;
 
     @PostMapping("/acc/login")
-    public ResponseEntity<?> getAcc(@RequestBody Account a) {
+    public ResponseEntity<?> login(@RequestBody Account a) {
         return as.check(a);
     }
 
